a69b576d5c1c411d154ec220b3cfa55e7b44e608
Jaehyun(Jay) Shim <jaehyunshim85@gmail.com>
2022-06-16 09:12:09 -0600

    added h3

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <header className="App-header">
         <h1>working?</h1>
         <h2>checking it one more time after git</h2>
+        <h3>is it updating?</h3>
       </header>
     </div>
   );
